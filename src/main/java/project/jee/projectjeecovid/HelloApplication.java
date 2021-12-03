fd888d79ea7e54167ae39cf952c1dabe4f230c68
Sven <svenclementoff@gmail.com>
2021-12-03 17:52:56 +0100

    feat: master project

---
+++ src/main/java/project/jee/projectjeecovid/HelloApplication.java
@@ -0,0 +1,9 @@
+package project.jee.projectjeecovid;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
